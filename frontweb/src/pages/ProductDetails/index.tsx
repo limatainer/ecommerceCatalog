fc5d99590cf58ee92d28248f14bfa676ecb54a56
Mariana Cousseiro de Lima <limatainer@MBP-de-Mariana.lan>
2022-01-06 19:09:30 +0000

    hooks

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,27 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import axios from 'axios';
 import ProductPrice from 'components/ProductPrice';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Product } from 'types/product';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css'
 
+type UrlParams = {
+  productId: string;
+}
+
 const ProductDetails = () => {
+
+  const { productId } = useParams<UrlParams>();
+  const [product, setProduct] = useState<Product>();
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`).then(response => {
+      setProduct(response.data);
+    });
+  }, [productId]);
+
   return (
     <div className="product-details-container">
       <div className="base-card product-details-card">
@@ -16,16 +33,16 @@ const ProductDetails = () => {
         </div>
         <div className="row">
           <div className="col-xl-6">
-            <div className="img-container"><img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do Produto" /></div>
+            <div className="img-container"><img src={product?.imgUrl} alt={product?.name} /></div>
             <div className="name-price-container">
-              <h1>Nome do Produto</h1>
-              <ProductPrice price={2365.4} />
+              <h1>{product?.name}</h1>
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
               <h2>Descrição do Produto</h2>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus, asperiores?</p>
+              <p>{product?.description}</p>
             </div>
           </div>
         </div>
