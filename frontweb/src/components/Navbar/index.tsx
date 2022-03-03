35e2f14d1459e4d47b9ad9f1aa35239ad6852ecf
Mariana Cousseiro de Lima <limatainer@MBP-de-Mariana.lan>
2022-03-03 00:28:09 +0000

    styling

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -69,7 +69,7 @@ function Navbar() {
         <div className='nav-login-logout'>
           {authData.authenticated ? (
             <>
-              <span>{authData.tokenData?.user_name}</span>
+              <span className='nav-username'>{authData.tokenData?.user_name}</span>
               <a href="#logout" onClick={handleLogoutClick}>LOGOUT</a>
             </>
           ) : (
