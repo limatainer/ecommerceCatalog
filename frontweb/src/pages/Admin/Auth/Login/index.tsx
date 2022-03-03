f5489115640113ef1758d4b40e29b32b4c6e29d7
Mariana Cousseiro de Lima <limatainer@MBP-de-Mariana.lan>
2022-03-03 19:02:11 +0000

    location replace history

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,4 +1,4 @@
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm, SubmitHandler } from "react-hook-form";
 import './styles.css';
@@ -11,7 +11,14 @@ type FormData = {
   password: string,
 };
 
+type LocationState = {
+  from: string
+}
+
 const Login = () => {
+  const location = useLocation<LocationState>();
+  const { from } = location.state || { from: { pathname: '/admin' } }
+
   const { setAuthContextData } = useContext(AuthContext)
   const [hasError, setHasError] = useState(false);
   const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
@@ -28,7 +35,7 @@ const Login = () => {
         authenticated: true,
         tokenData: getTokenData()
       })
-      history.push('/admin')
+      history.replace(from) //ele vai subistituir a rota do login pela do que eu tava via endereco
     }).catch((error) => {
       setHasError(true) //isto eh para trabalhar a mensagem de erro condicional e esta configurado na div embaixo
       // console.log('ERROR', error)
