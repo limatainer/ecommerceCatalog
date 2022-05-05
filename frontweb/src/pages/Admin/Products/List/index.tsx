fcf2e5265b1ee513df2e535594f4ff4c33509b39
Mariana Cousseiro de Lima <limatainer@MacBook-Pro-de-Mariana.local>
2022-05-05 22:49:20 +0100

    admin buttons

--- frontweb/src/pages/Admin/Products/List/index.tsx
+++ frontweb/src/pages/Admin/Products/List/index.tsx
@@ -30,7 +30,7 @@ export default function List() {
 
 
   return (
-    <>
+    <div className='product-crud-container'>
       <div className="product-crud-bar-container">
         <Link to="/admin/products/create">
           <button className='btn btn-primary text-white btn-crud-add'>ADICIONAR</button>
@@ -49,6 +49,6 @@ export default function List() {
           <ProductCrudCard product={product} />
         </div>
       </div>
-    </>
+    </div>
   )
 }
