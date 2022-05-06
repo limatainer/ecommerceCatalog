a379f6e4110e3e390d997be1e7c82659fd8d7f79
Mariana Cousseiro de Lima <limatainer@MacBook-Pro-de-Mariana.local>
2022-05-06 21:17:57 +0100

    admin form

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import './styles.css'
 
 export default function Form() {
   return (
-    <h1>Form</h1>
+    <div className='product-crud-container'>
+      <div className='base-card product-crud-form-card'>
+        <h1 className='product-crud-form-title'>DADOS DO PRODUTO</h1>
+
+        <form action="">
+
+          <div className="row product-crud-inputs-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className='margin-bottom-30'>
+                <input type="text" className='form-control base-input' />
+              </div>
+              <div className='margin-bottom-30'>
+                <input type="text" className='form-control base-input' />
+              </div>
+              <div>
+                <input type="text" className='form-control base-input' />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div>
+                <textarea name="" rows={10} className='form-control base-input h-auto'></textarea>
+              </div>
+            </div>
+          </div>
+
+          <div className='product-crud-buttons-container'>
+            <button className='btn btn-outline-danger product-crud-button'>CANCELAR</button>
+            <button className='btn btn-primary product-crud-button text-white'>SALVAR</button>
+          </div>
+        </form>
+
+      </div>
+
+    </div>
   )
 }
