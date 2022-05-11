f8235377ccd3df57f9495da21f903c7e797d3543
Mariana Cousseiro de Lima <limatainer@MacBook-Pro-de-Mariana.local>
2022-05-11 13:27:38 +0100

    admin crud

--- frontweb/src/pages/Admin/Products/Form/index.tsx
+++ frontweb/src/pages/Admin/Products/Form/index.tsx
@@ -1,27 +1,33 @@
 import { AxiosRequestConfig } from 'axios';
 import React from 'react'
 import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 import { Product } from 'types/product';
 import { requestBackend } from 'util/requests';
 import './styles.css'
 
 export default function Form() {
+  const history = useHistory()
   const { register, handleSubmit, formState: { errors } } = useForm<Product>();
 
-
   const onSubmit = (formData: Product) => {
-
+    const data = {
+      ...formData,
+      imgURL: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg",
+      categories: [{ id: 1, name: "" }],
+    }
     const config: AxiosRequestConfig = {
       method: 'POST',
       url: '/products',
-      data: formData,
+      data,
       withCredentials: true
     }
-
     requestBackend(config).then((response) => {
       console.log(response.data)
     })
-
+  }
+  const handleCancel = () => {
+    history.push("/admin/products")
   }
 
   return (
@@ -46,21 +52,37 @@ export default function Form() {
                 <div className="invalid-feedback d-block">{errors.name?.message}</div>
               </div>
               <div className='margin-bottom-30'>
-                <input type="text" className='form-control base-input' />
-              </div>
-              <div>
-                <input type="text" className='form-control base-input' />
+                <input
+                  {...register("price", {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${errors.price ? 'is-invalid' : ''}`}
+                  placeholder="Preço"
+                  name="price"
+                />
+                <div className="invalid-feedback d-block">{errors.price?.message}</div>
               </div>
             </div>
             <div className="col-lg-6">
               <div>
-                <textarea name="" rows={10} className='form-control base-input h-auto'></textarea>
+                <textarea rows={10}
+                  {...register("description", {
+                    required: 'Campo obrigatório',
+                  })}
+                  className={`form-control base-input h-auto ${errors.price ? 'is-invalid' : ''}`}
+                  placeholder="Descrição"
+                  name="description"
+                />
+                <div className="invalid-feedback d-block">{errors.description?.message}</div>
               </div>
             </div>
           </div>
 
           <div className='product-crud-buttons-container'>
-            <button className='btn btn-outline-danger product-crud-button'>CANCELAR</button>
+            <button
+              onClick={handleCancel}
+              className='btn btn-outline-danger product-crud-button'>CANCELAR</button>
             <button className='btn btn-primary product-crud-button text-white'>SALVAR</button>
           </div>
         </form>
